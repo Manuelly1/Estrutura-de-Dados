d27498b3f78f615b5d07d3cc3ef9ee98f0a52b0d
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-05-19 22:27:31 -0300

    update no selection

--- selection/selection_sort.c
+++ selection/selection_sort.c
@@ -27,44 +27,6 @@ void selection_sort(int *v, unsigned int n) {
     }
 }
 
-#include <stdio.h>
-#include <stdlib.h> 
-#include <time.h>
-#include <sys/time.h>
-
-void merge_sort(int *V, int s, int e);
-void merge(int *V, int s, int e, int m);
-
-void merge_sort(int *V, int s, int e) {
-    int m;
-    if(s < e) {
-        m = ((s+e) / 2);
-        merge_sort(V, s, m);
-        merge_sort(V, m+1, e);
-        merge(V, s, m, e);
-    }
-}
-
-void merge(int *V, int s, int e, int m) {
-    int i = s;
-    int j = m + 1;
-    int *M = (int *) malloc((e-s+1) * sizeof(int));
-
-    for(int k = 0; k <= (e-s); k++) {
-        if((j>e) || ((i <= m) && (V[i] < V[j]))) {
-            M[k] = V[i];
-            i = i + 1;
-        } else {
-            M[k] = V[j];
-            j = j + 1;
-        }
-    }
-
-    for(int k = 0; k <= (e-s); k++) {
-        V[s+k] = M[k];
-    }
-    free(M);
-}
 
 int main(int argc, char **argv) {
     struct timespec a, b;
